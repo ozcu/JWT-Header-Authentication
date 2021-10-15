67cf79c676dd76f442b82d5f84ebc9826bb565f3
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-15 22:22:57 +0300

    add logout method

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,43 @@
 <template>
   
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/register">Sign Up</router-link>
+      <router-link to="/">Home</router-link> 
+      <router-link v-show="!user" to="/login">Login</router-link> 
+      <router-link class="registerLink" v-show="!user" to="/register">Sign Up</router-link> 
+      <!-- <router-link class="logoutLink"  @click="logoutHandle" to="/">Logout</router-link> -->
+      <a href="javascript:void(0)" @click="logoutHandle" v-show="user" > Logout </a>
     </div>
 </template>
 
 <script>
-
+import {mapState} from 'vuex'
 export default {
-name:'Nav'
+name:'Nav',
+computed:{
+  ...mapState(['user'])
+},
+methods:{
+  logoutHandle(){
+
+    localStorage.removeItem('token')
+    this.$store.state.user =null
+   this.$router.push('/').catch(()=>{})
+   
+  }
+}
 }
 </script>
 
 <style>
+#nav{
+  padding:10px;
+  display:grid;
+  margin:0 auto;
+  grid-template-columns:repeat(3,1fr);
 
+}
+#nav router-link{
+padding:30px;
+
+}
 </style>
