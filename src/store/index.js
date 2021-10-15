ca631dea391b7a4dcaa8384d4d05003119e21dc3
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-15 22:22:17 +0300

    add global user state

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    user:null
+  },
+  mutations: {
+    
+  },
+  actions: {
+    
+  },
+ 
+  getters:{
+
+  },
+     
+    
+  modules: {
+
+  }
+
+
+  
 });
