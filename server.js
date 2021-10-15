3ec1a534509156ea36bafc9ad32bdc714c9275ed
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-10-15 22:23:45 +0300

    route specific user

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const jsonServer = require('json-server')
-
 const jwt = require('jsonwebtoken')
+
 
 
 
@@ -13,7 +13,6 @@ const userdb = JSON.parse(fs.readFileSync('./db.json','utf-8'))
 
 server.use(jsonServer.defaults())
 server.use(jsonServer.bodyParser)
-
 
 
 const SECRET_KEY = '12345'
@@ -38,25 +37,36 @@ function isAuthenticated({email,password}){
    return usersChecked
     
 }
-
+//check user and crate JWT
 server.post('/auth/login',(req,res)=>{
 
     const {email,password} =req.body
     console.log({email,password})
     
     if(isAuthenticated({email,password})===false){
-      
-         const status =401
+        const status =401
         const message ='Incorret email or password'
         res.status(status).json({status,message}) 
         return
     }
     else{
+        const newUser = userdb.users.find(user =>user.email===email)
         const access_token = createToken({email,password})
-        res.status(200).json({access_token})
+        res.status(200).json({access_token:access_token,user:newUser})
         
     }
 })
+
+server.get('/users/:id', async (req, res) => {
+    try {
+      const user = userdb.users.findIndex(user=>user.id === req.params.id) 
+      res.render(user)
+    } catch (e) {
+      return res.status(404).send('Cannot find user!')
+    }
+  })
+
+
 
 server.use(/^(?!\/auth).*$/,  (req, res, next) => {
     if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
